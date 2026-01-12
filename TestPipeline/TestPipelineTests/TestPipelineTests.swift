c95165fa5233b69e229732d66f10cd0943f2af6c
Krishna Prasad <skprasadu@yahoo.com>
2026-01-12 05:49:01 -0800

    Track TestPipeline as normal folder (remove broken submodule)

---
+++ TestPipeline/TestPipelineTests/TestPipelineTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestPipelineTests.swift
+//  TestPipelineTests
+//
+//  Created by Krishna Srinivasamurthy on 1/12/26.
+//
+
+import Testing
+@testable import TestPipeline
+
+struct TestPipelineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
